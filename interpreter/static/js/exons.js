f0941f28e34cf6fd75415ca62cd7aa1d2a85b3fe
Ivo Fokkema <Ivo@IFokkema.nl>
2024-07-30 11:24:37 +0200

    Fix indentation in exons.js which was a mix of tabs and spaces.
    Now, only spaces are used, like in all the other JS files.

--- interpreter/static/js/exons.js
+++ interpreter/static/js/exons.js
@@ -1,56 +1,56 @@
 $(window).bind("pageshow", function() {
-	$(".userAnswer").removeAttr("disabled");
-  	document.getElementById('userAnswer').value = "";
-  	$("#togglertext").click();
-	});
+    $(".userAnswer").removeAttr("disabled");
+    document.getElementById('userAnswer').value = "";
+    $("#togglertext").click();
+});
 
 var exons = [];
 
 $(document).ready(function(){
-	$('img').click(function() {
+    $('img').click(function() {
         $("[id^=radio]").prop("checked",false);
-    	var id = "#".concat(this.id)
-    	var val = parseInt(this.id)
-    	var temp = ''
-    	exons.push(val)
-    	//$(id).fadeTo(1000,0.1);
-		var largest = 0;
-		var smallest = 80;
-		for (i=0; i<=exons.length;i++){
-    		if (exons[i]>largest) {
-        		var largest = parseInt(exons[i]);
-    			}
-    		if (exons[i]<smallest) {
-    			smallest = parseInt(exons[i]);
-    		}
-		}
-    	if (smallest === largest){
-    		temp = "Deletion of exon "
- 			temp = temp.concat(this.id)
- 		}
- 		else if (smallest !== largest){
- 			temp = "Deletion of exons "
- 			temp = temp +smallest + "-"+ largest
- 		}
- 		for (i=smallest;i<=largest;i++){
- 			id = "#" + i
- 			$(id).fadeTo(1000,0.1);
- 		}
-    	$(".userAnswer").val(temp)
-    	$(".userAnswer").attr("disabled","disabled");
-	});
-	
-	$("#reset").click(function(){
-        $("[id^=radio]").prop("checked",false);
-		$("img").fadeTo(1000,1);
-		$(".userAnswer").val("");
-		$(".userAnswer").removeAttr("disabled");
-        exons.length = 0;
-	});
+        var id = "#".concat(this.id)
+        var val = parseInt(this.id)
+        var temp = ''
+        exons.push(val)
+        //$(id).fadeTo(1000,0.1);
+        var largest = 0;
+        var smallest = 80;
+        for (i=0; i<=exons.length;i++){
+            if (exons[i]>largest) {
+                var largest = parseInt(exons[i]);
+            }
+            if (exons[i]<smallest) {
+                smallest = parseInt(exons[i]);
+            }
+        }
+        if (smallest === largest){
+            temp = "Deletion of exon "
+            temp = temp.concat(this.id)
+        }
+        else if (smallest !== largest){
+            temp = "Deletion of exons "
+            temp = temp +smallest + "-"+ largest
+        }
+        for (i=smallest;i<=largest;i++){
+            id = "#" + i
+            $(id).fadeTo(1000,0.1);
+        }
+        $(".userAnswer").val(temp)
+        $(".userAnswer").attr("disabled","disabled");
+    });
 
-	$(".btn-primary").click(function(){
-		$(".userAnswer").removeAttr("disabled");
-	});
+    $("#reset").click(function(){
+        $("[id^=radio]").prop("checked",false);
+        $("img").fadeTo(1000,1);
+        $(".userAnswer").val("");
+        $(".userAnswer").removeAttr("disabled");
+        exons.length = 0;
+    });
+
+    $(".btn-primary").click(function(){
+        $(".userAnswer").removeAttr("disabled");
+    });
     $("[id^=radio]").click(function(){
         $(".userAnswer").val($(this).val());
     });
